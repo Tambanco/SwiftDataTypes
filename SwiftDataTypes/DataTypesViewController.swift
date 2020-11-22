a087cdba9f6f2388e27ceb499f5199d5ad5ebd77
Tambanco <tmbnc@bk.ru>
2020-11-22 14:38:45 +0300

    minor commit

--- SwiftDataTypes/DataTypesViewController.swift
+++ SwiftDataTypes/DataTypesViewController.swift
@@ -332,4 +332,20 @@ extension UIColor
    }
 }
 
+//MARK: - Set gradient color
+extension UIView
+{
+    func setGradientBackground(_ colorOne: UIColor, _ colorTwo: UIColor)
+    {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
 
