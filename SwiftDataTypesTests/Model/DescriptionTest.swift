48a19597f3a7abd32f5241b8aa6030b9a8540e8b
Al <alexunder_5542@mail.ru>
2021-05-20 15:50:18 +0300

    added testInitDescrioptionWithTitleInt8

---
+++ SwiftDataTypesTests/Model/DescriptionTest.swift
@@ -0,0 +1,23 @@
+//
+//  DescriptionTest.swift
+//  SwiftDataTypesTests
+//
+//  Created by tambanco 🥳 on 20.05.2021.
+//  Copyright © 2021 Tambanco. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftDataTypes
+
+class DescriptionTest: XCTestCase
+
+{
+    func testInitDescrioptionWithTitleInt8()
+    {
+        let description = Description()
+        
+        XCTAssertNotNil(description)
+    }
+
+
+}
