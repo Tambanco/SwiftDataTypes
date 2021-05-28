cdfeac413fbcff1b796396a9ce8aa9c529b805ca
Al <alexunder_5542@mail.ru>
2021-05-28 14:34:22 +0300

    added Buttons and Text structures

---
+++ SwiftDataTypes/Model/Buttons.swift
@@ -0,0 +1,17 @@
+//
+//  Buttons.swift
+//  SwiftDataTypes
+//
+//  Created by tambanco 🥳 on 28.05.2021.
+//  Copyright © 2021 Tambanco. All rights reserved.
+//
+
+import Foundation
+
+let buttons = [
+    ["Int8", "UInt8", "Bool"],
+    ["Int16", "UInt16", "String"],
+    ["Int32", "UInt32", "Character"],
+    ["Int64", "UInt64", "Optional"],
+    ["Float", "Double", "Tuples"]
+]
